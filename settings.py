da83be0d76b71aefe30334a314587fb85eec4db7
Kartik-Tyagi1 <kartiktyagi278@gmail.com>
2023-04-14 16:27:00 -0400

    first commit - created map and player movement

---
+++ settings.py
@@ -0,0 +1,11 @@
+# Game Settings
+
+# Screen Resolution and FPS
+RES = WIDTH, HEIGHT = 1600, 900
+FPS = 60
+
+# Player Positioning
+PLAYER_POS = 1.5, 5 # mini map
+PLAYER_ANGLE = 0
+PLAYER_SPEED = 0.004
+PLAYER_ROT_SPEED = 0.002
